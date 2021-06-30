f3119ae19a8017b5a1a5a945b0de316bc69cfe06
jpb06 <jpb.06@outlook.com>
2021-06-30 21:48:35 +0200

    Jest base setup

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const {
+   compilerOptions: { paths: tsconfigPaths },
+} = require("./tsconfig");
+
+module.exports = {
+   roots: ["<rootDir>/src/"],
+   globalSetup: "<rootDir>/jest.setup.env.ts",
+   moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+   testEnvironment: "jsdom",
+   testPathIgnorePatterns: [
+      "<rootDir>[/\\\\](node_modules|.next|coverage)[/\\\\]",
+   ],
+   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+   transform: {
+      "^.+\\.(ts|tsx)$": "babel-jest",
+   },
+   watchPlugins: [
+      "jest-watch-typeahead/filename",
+      "jest-watch-typeahead/testname",
+   ],
+   moduleNameMapper: {
+      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+      "\\.(gif|ttf|eot|svg|png)$": "test-file-stub",
+      ...pathsToModuleNameMapper(tsconfigPaths, { prefix: "<rootDir>/src" }),
+   },
+   coveragePathIgnorePatterns: [
+      "<rootDir>/src/tests-related/",
+      "<rootDir>/src/types/",
+      "<rootDir>/src/pages/",
+   ],
+   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+   coverageReporters: ["json-summary", "text", "lcov"],
+};
