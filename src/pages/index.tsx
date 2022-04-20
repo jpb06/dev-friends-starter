d1daa0379cde4896302de9a65041837986fa486b
JP Bois <jp.bois.06@outlook.fr>
2022-04-20 15:49:34 +0200

    Switching to react 18 (#9)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Home as HomeComponent } from '@components/epic/home/Home';
+import { Home as HomeComponent } from '@components/home/Home';
 
 export const Home: React.FC = () => <HomeComponent />;
 
